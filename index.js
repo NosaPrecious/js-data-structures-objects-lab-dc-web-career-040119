f35e4e658f1c35346be903cd6a19150226bfd809
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-18 15:57:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function  destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   return Object.assign(driver, {[key] : value});
 }
 
-function deleteFromDriverByKey(driver, key, value){
+function deleteFromDriverByKey(driver, key){
   var objVal = driver[key];
   var newObj = destructivelyUpdateDriverWithKeyAndValue(driver, key, objVal)
   delete newObj.key;
